cd44a8fd59c5fb0de4b7d6b6157d44a35301c216
Khumoyun098 <xumoyun.098@mail.ru>
2021-08-06 02:45:01 +0500

    settings update

--- core/settings.py
+++ core/settings.py
@@ -135,5 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
